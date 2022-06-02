d8668cc79276d93924dc23effe145f7fb8fe5450
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-06-02 14:20:23 -0400

    ex1

---
+++ class2-ex1.py
@@ -0,0 +1,13 @@
+triple_quote_string = '''In this alphabetical list of academic programs, 
+each program is followed by the name of its sponsoring school or institute. 
+The glossary explains any abbreviations used in this list.
+'''
+
+# immutability: string is not changeable
+
+my_string = "Peter"
+# my_string[0] = 'J' <--- will give you a typeError
+
+my_string = "Brand New String"
+
+print(my_string)
